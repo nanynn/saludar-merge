4174d2dd17717a1bf4b4858cc66c9679c4314ad7
caroquintana <caro.quintana.n@gmail.com>
2017-05-08 13:01:38 -0400

    ahora si, te destrozo

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ function saludar (){
 	console.log("hola mundo");
 }
 
-
 //Añadiendo un título
 var elemento = document.createElement("h2");
 var contenido = document.createTextNode("A-TÓ-MI-CO SQUAD");
