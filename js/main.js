5dc3b117e4f9785c1bfe0b4535fadcf540d84287
caroquintana <caro.quintana.n@gmail.com>
2017-05-08 12:58:23 -0400

    hola, aqui te destrozo XD

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 function saludar (){
 	console.log("hola mundo");
 }
+
+
+//Añadiendo un título
+var elemento = document.createElement("h2");
+var contenido = document.createTextNode("A-TÓ-MI-CO SQUAD");
+elemento.appendChild(contenido);
+elemento.setAttribute("align", "center");
+document.getElementById("titulo").appendChild(elemento);
