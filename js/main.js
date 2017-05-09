d836aeff3739e4b550f003ddc737b6c139cbfdb6
Anayn Pavez <nanyn.pavez@gmail.com>
2017-05-08 23:31:40 -0300

    version funcional sin conflictos

--- js/main.js
+++ js/main.js
@@ -9,4 +9,4 @@ var contenido = document.createTextNode("A-TÓ-MI-CO SQUAD");
 elemento.appendChild(contenido);
 elemento.setAttribute("align", "center");
 document.getElementById("titulo").appendChild(elemento);
->>>>>>> 4174d2dd17717a1bf4b4858cc66c9679c4314ad7
+
